b5dc04e4eb5867ff1855180efe99f4e67588cc6f
Vesselin Jivkov <jivkovvesselin@gmail.com>
2024-09-01 15:24:37 +0300

    Update navigation styling and switch to Lexend font
    - Updated navigation bar styling with new border, padding, and text adjustments.
    - Added "TrainLedger" text next to the logo in the navigation bar.
    - Changed font from Inter to Lexend across the application.
    - Applied dark background color to the body in index.html.

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -24,10 +24,13 @@ function classNames(...classes: string[]): string {
 
 export default function Nav(): ReactElement {
 	return (
-		<Disclosure as='nav' className='bg-gray-700'>
-			<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-				<div className='relative flex h-16 items-center justify-between'>
-					<div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+		<Disclosure
+			as='nav'
+			className='border-b border-solid border-b-[#223449] px-10 py-3'
+		>
+			<div className='mx-auto px-2 sm:px-6 lg:px-8'>
+				<div className='relative flex items-center justify-between'>
+					<div className='relative inset-y-0 left-0 flex items-center sm:hidden'>
 						{/* Mobile menu button */}
 						<DisclosureButton className='group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
 							<span className='absolute -inset-0.5' />
@@ -42,14 +45,21 @@ export default function Nav(): ReactElement {
 							/>
 						</DisclosureButton>
 					</div>
-					<div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
-						<div className='flex flex-shrink-0 items-center'>
-							<img
-								alt='Your Company'
-								src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-								className='h-8 w-auto'
-							/>
+					<div className='flex items-center gap-4 text-white'>
+						<div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-center'>
+							<div className='flex flex-shrink-0 items-center'>
+								<img
+									alt='Your Company'
+									src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
+									className='h-8 w-auto'
+								/>
+								<div className='ml-2 text-lg font-bold text-white'>
+									TrainLedger
+								</div>
+							</div>
 						</div>
+					</div>
+					<div className='flex min-w-6 justify-end gap-8 md:flex-1'>
 						<div className='hidden sm:ml-6 sm:block'>
 							<div className='flex space-x-4'>
 								{navigation.map(item => (
@@ -59,9 +69,9 @@ export default function Nav(): ReactElement {
 										aria-current={item.current ? 'page' : undefined}
 										className={classNames(
 											item.current
-												? 'bg-gray-900 text-white'
-												: 'text-gray-300 hover:bg-gray-700 hover:text-white',
-											'rounded-md px-3 py-2 text-sm font-medium'
+												? 'bg-gray-700 text-white'
+												: 'text-white hover:bg-gray-700 hover:text-white',
+											'rounded-md px-3 py-2 text-sm font-medium leading-normal'
 										)}
 									>
 										{item.name}
@@ -69,60 +79,60 @@ export default function Nav(): ReactElement {
 								))}
 							</div>
 						</div>
-					</div>
-					<div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-						<button
-							type='button'
-							className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-						>
-							<span className='absolute -inset-1.5' />
-							<span className='sr-only'>View notifications</span>
-							<BellIcon aria-hidden='true' className='h-6 w-6' />
-						</button>
-
-						{/* Profile dropdown */}
-						<Menu as='div' className='relative ml-3'>
-							<div>
-								<MenuButton className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-									<span className='absolute -inset-1.5' />
-									<span className='sr-only'>Open user menu</span>
-									<img
-										alt=''
-										src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-										className='h-8 w-8 rounded-full'
-									/>
-								</MenuButton>
-							</div>
-							<MenuItems
-								transition
-								className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
+						<div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+							<button
+								type='button'
+								className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
 							>
-								<MenuItem>
-									<a
-										href='/#'
-										className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100'
-									>
-										Your Profile
-									</a>
-								</MenuItem>
-								<MenuItem>
-									<a
-										href='/#'
-										className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100'
-									>
-										Settings
-									</a>
-								</MenuItem>
-								<MenuItem>
-									<a
-										href='/#'
-										className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100'
-									>
-										Sign out
-									</a>
-								</MenuItem>
-							</MenuItems>
-						</Menu>
+								<span className='absolute -inset-1.5' />
+								<span className='sr-only'>View notifications</span>
+								<BellIcon aria-hidden='true' className='h-6 w-6' />
+							</button>
+
+							{/* Profile dropdown */}
+							<Menu as='div' className='relative ml-3'>
+								<div>
+									<MenuButton className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+										<span className='absolute -inset-1.5' />
+										<span className='sr-only'>Open user menu</span>
+										<img
+											alt=''
+											src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+											className='h-8 w-8 rounded-full'
+										/>
+									</MenuButton>
+								</div>
+								<MenuItems
+									transition
+									className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
+								>
+									<MenuItem>
+										<a
+											href='/#'
+											className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100'
+										>
+											Your Profile
+										</a>
+									</MenuItem>
+									<MenuItem>
+										<a
+											href='/#'
+											className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100'
+										>
+											Settings
+										</a>
+									</MenuItem>
+									<MenuItem>
+										<a
+											href='/#'
+											className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100'
+										>
+											Sign out
+										</a>
+									</MenuItem>
+								</MenuItems>
+							</Menu>
+						</div>
 					</div>
 				</div>
 			</div>
